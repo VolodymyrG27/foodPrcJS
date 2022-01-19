a7041a11a640fac397812aee8ad9cfaaf207da1c
Volodymyr Gotsak <vgotsak29@gmail.com>
2022-01-19 15:07:51 +0200

    Sending data to the server using fetch

--- js/script.js
+++ js/script.js
@@ -252,33 +252,36 @@ window.addEventListener('DOMContentLoaded', () => {
 
             form.insertAdjacentElement('afterend', statusMessage); //Замість append для форми яка зразу відображена
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json'); //Сервер має приймати дані в JSON
-
            const formData = new FormData(form);
            
-
-           /*Form Data перетворюємо в json*/
+           // Form Data перетворюємо в json
             const object = {};
             formData.forEach(function(value, key) {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
+            //Відправка даних на сервер за допомогою fetch
 
-            request.send(json);
-
-           request.addEventListener('load', () => {
-            if (request.status === 200) {
-                console.log(request.response);
+            fetch('server1.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
                 showThanksModal(message.success);
                 form.reset();
                 statusMessage.remove();
-            } else {
+            })
+            .catch(() => {
                 showThanksModal(message.failure);
-            }
-           });
+            })
+            .finally(() => {
+                form.reset();
+            });
         });
     }
 
@@ -304,8 +307,8 @@ window.addEventListener('DOMContentLoaded', () => {
             prevModalDialog.classList.remove('hide');
             hideModal();
         }, 4000) 
-
     }
+
 
 
 
