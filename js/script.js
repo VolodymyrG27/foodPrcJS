50f23bc9bc6880f2b2791989ed39f2fdac67de79
Volodymyr Gotsak <vgotsak29@gmail.com>
2022-01-20 14:03:32 +0200

    Creating cards without using a class

--- js/script.js
+++ js/script.js
@@ -202,14 +202,39 @@ window.addEventListener('DOMContentLoaded', () => {
 
         return await res.json();
     };
-
+/*
     getResource('http://localhost:3000/menu')
         .then(data => {
             data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
-        });
+        });*/
+
+        //другий варіант створення вище вказаних елементів, різниця в тому що він не буде використовувати класи, буде зразу формути верстку
+        getResource('http://localhost:3000/menu')
+        .then(data => createCard(data));
+
+        function createCard(data) {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                const element = document.createElement('div');
+
+                element.classList.add('menu__item');
+                element.innerHTML = `
+                    <img src="${img}" alt="${altimg}">
+                    <h3 class="menu__item-subtitle">${title}"</h3>
+                    <div class="menu__item-descr">${descr}></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${price}</span> грн/день</div>
+                    </div>
+                `;
+
+                document.querySelector('.menu .container').append(element);
+            });
+        }
     
+    
+
     /*Відправляємо дані на сервер з форм*/
     const forms = document.querySelectorAll('form');
 
